8539a72e7e3cdb01f4002b59d8c76f93a423c8dd
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2024-03-17 10:26:21 +0200

    Upload files needed

---
+++ data.pl
@@ -0,0 +1,5 @@
+customer(104, mazen122).
+item(pepsi, 'PepsiCo', 8.5).
+order(1,1,[ariel_4k, biskrem, cheese]).
+boycott_company('PepsiCo', 'PepsiCo purchased the ...').
+alternative(chipsy, tiger).
